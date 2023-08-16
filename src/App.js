c2f4443681b38c57cc8b1a1c2bfe3d6ba5d9fbb1
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-08-15 22:53:49 -0600

    Add Table component
    - Implement table component, with js and css inside its own file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./assets/investment-calculator-logo.png";
 import Form from "./Components/Form";
+import Table from "./Components/Table";
 
 function App() {
   const calculateHandler = (userInput) => {
@@ -35,32 +36,10 @@ function App() {
         <img src={logo} alt="logo" />
         <h1>Investment Calculator</h1>
       </header>
-
       <Form />
-
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
-
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table />
     </div>
   );
 }
